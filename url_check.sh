f029ace8f6dda9492c04d3f47397d82cbc3317a2
root <root@localhost.localdomain>
2017-03-08 05:56:45 +0800

    add url check is ok

---
+++ url_check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function usage() {
+    echo $"usage:$0 url"
+    exit 1
+}
+
+function check_url() {
+    wget --spider -q -o /dev/null --tries=1 -T 5 $1
+    if [ $? -eq 0 ];then
+        echo "$1 is yes."
+    else
+        echo "$1" is no.
+    fi
+}
+
+function main() {
+    if [ $# -ne 1 ];then
+        usage
+    fi
+    check_url $1
+}
+main $*
